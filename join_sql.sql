9cac4908bd2b4e2d57e4c0a06a7d8862609d8254
dasileker <dasileker@gmail.com>
2020-10-29 11:37:16 +0100

    added the players table + the teames tables + name

--- join_sql.sql
+++ join_sql.sql
@@ -1,6 +1,6 @@
 SELECT matchid, player
 FROM goal
-WHERE teamid = 'MAR';
+WHERE teamid = 'GER';
 
 
 SELECT id, stadium, team1, team2
@@ -10,10 +10,26 @@ WHERE id = 1012;
 SELECT player, teamid, stadium, mdate
 FROM goal JOIN game
 ON game.id = goal.matchid
-WHERE goal.teamid = 'MAR'
+WHERE goal.teamid = 'GER '
 
 
 SELECT team1, team2, player
 FROM goal JOIN game
 ON goal.matchid = game.id
-WHERE goal.player LIKE 'Ziach%';
+WHERE goal.player LIKE 'Mario%';
+
+SELECT player, teamid, coach, gtime
+FROM goal JOIN eteam
+ON goal.teamid = eteam.id
+WHERE gtime <= 10;
+
+SELECT mdate, teamname
+FROM game JOIN eteam
+ON game.team1 = eteam.id
+WHERE coach = 'Fernando Santos';
+
+
+SELECT player
+FROM goal JOIN game
+ON goal.matchid = game.id
+WHERE game.stadium = 'National Stadium, Warsaw';
