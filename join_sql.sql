efa2a77fe5169a8c03f3cf4f64913cac2407239e
dasileker <dasileker@gmail.com>
2020-10-30 08:59:39 +0100

    added the second file

--- join_sql.sql
+++ join_sql.sql
@@ -1,22 +1,22 @@
 SELECT matchid, player
 FROM goal
-WHERE teamid = 'GER';
+WHERE teamid = 'Eng';
 
 
 SELECT id, stadium, team1, team2
 FROM game
-WHERE id = 1012;
+WHERE id = 1023;
 
 SELECT player, teamid, stadium, mdate
 FROM goal JOIN game
 ON game.id = goal.matchid
-WHERE goal.teamid = 'GER '
+WHERE goal.teamid = 'ENG '
 
 
 SELECT team1, team2, player
 FROM goal JOIN game
 ON goal.matchid = game.id
-WHERE goal.player LIKE 'Mario%';
+WHERE goal.player LIKE 'Wayne%';
 
 SELECT player, teamid, coach, gtime
 FROM goal JOIN eteam
@@ -26,19 +26,19 @@ WHERE gtime <= 10;
 SELECT mdate, teamname
 FROM game JOIN eteam
 ON game.team1 = eteam.id
-WHERE coach = 'Fernando Santos';
+WHERE coach = 'Fabio Capello';
 
 
 SELECT player
 FROM goal JOIN game
 ON goal.matchid = game.id
-WHERE game.stadium = 'National Stadium, Warsaw';
+WHERE game.stadium = 'Donbass Arena';
 
 
 SELECT DISTINCT player
 FROM goal JOIN game
 ON goal.matchid = game.id
-WHERE 'GER' IN (team1, team2) AND teamid <> 'GER';
+WHERE 'ENG' IN (team1, team2) AND teamid <> 'ENG';
 
 SELECT teamname, COUNT(player)
 FROM eteam JOIN goal
@@ -55,11 +55,11 @@ GROUP BY stadium;
 SELECT matchid, mdate, COUNT(player) AS goals_scored
 FROM game JOIN goal
 ON game.id = goal.matchid
-WHERE 'POL' IN (team1, team2)
+WHERE 'UKR' IN (team1, team2)
 GROUP BY matchid, mdate;
 
 SELECT matchid, mdate, COUNT(player)
 FROM game JOIN goal
 ON game.id = goal.matchid
-WHERE goal.teamid = 'GER'
+WHERE goal.teamid = 'ENG'
 GROUP BY matchid, mdate;
